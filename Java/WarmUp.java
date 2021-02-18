8357846e61d7ca183e6156087554ec88cc11cfd5
TkDazzler <40868910+Tanmai2002@users.noreply.github.com>
2021-02-18 23:07:03 +0530

    Update WarmUp.java

--- Java/WarmUp.java
+++ Java/WarmUp.java
@@ -297,6 +297,22 @@ public boolean startHi(String str) {
   return str.startsWith("hi");
 }
 
+///////////////     17   ///////////////////////
+/*
+
+Given two temperatures, return true if one is less than 0 and the other is greater than 100.
+
+
+icyHot(120, -1) → true
+icyHot(-1, 120) → true
+icyHot(2, 120) → false
+*/
+
+///////////////////////////////////////////////////////
+public boolean icyHot(int temp1, int temp2) {
+  return (temp1*temp2<0 && (temp1>100 || temp2>100));
+}
+
 
 
 
