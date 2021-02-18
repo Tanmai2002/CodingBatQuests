90a66986f3dfe6b9b6a13d9e87c54dc8b5cb3836
TkDazzler <40868910+Tanmai2002@users.noreply.github.com>
2021-02-18 22:59:39 +0530

    Update WarmUp.java

--- Java/WarmUp.java
+++ Java/WarmUp.java
@@ -256,6 +256,31 @@ public boolean or35(int n) {
   return ((n%3==0)|| (n%5==0));
 }
 
+///////////////     15   ///////////////////////
+/*
+
+
+Given a string, take the first 2 chars and return the string with the 2 chars 
+added at both the front and back, so "kitten" yields"kikittenki".
+If the string length is less than 2, use whatever chars are there.
+
+
+front22("kitten") → "kikittenki"
+front22("Ha") → "HaHaHa"
+front22("abc") → "ababcab"
+*/
+
+///////////////////////////////////////////////////////
+public String front22(String str) {
+  int l=str.length();
+  String s=str;
+  if(l>2){
+     s=str.substring(0,2);
+  }
+    return s+str+s;
+    
+  
+}
 
 
 
