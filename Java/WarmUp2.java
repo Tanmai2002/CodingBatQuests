f4c14a2d1ea0c96f196c7507f7a78d9aa66accf1
TkDazzler <40868910+Tanmai2002@users.noreply.github.com>
2021-02-21 21:07:51 +0530

    Update WarmUp2.java

--- Java/WarmUp2.java
+++ Java/WarmUp2.java
@@ -121,3 +121,171 @@ public String stringSplosion(String str) {
 
 }
 
+////////////////////////    7     ///////////////////////
+/*
+
+Given a string, return the count of the number of times that a substring length 2
+appears in the string and also as the last 2 chars of the string,
+so "hixxxhi" yields 1 (we won't count the end substring).
+
+
+last2("hixxhi") → 1
+last2("xaxxaxaxx") → 1
+last2("axxxaaxx") → 2
+*/
+//////////////////////////////////////////////////////////
+public int last2(String str) {
+  if(str.length()<3) return 0;
+  String s=str.substring(str.length()-2);
+  int l=0;
+  for (int i=0;i<str.length()-1;i++){
+    if(str.substring(i,i+2).equals(s))
+    l++;
+  }
+  return l-1;
+}
+
+
+////////////////////////    8     ///////////////////////
+/*
+Given an array of ints, return the number of 9's in the array.
+
+
+arrayCount9([1, 2, 9]) → 1
+arrayCount9([1, 9, 9]) → 2
+arrayCount9([1, 9, 9, 3, 9]) → 3
+*/
+//////////////////////////////////////////////////////////
+public int arrayCount9(int[] nums) {
+  int k=0;
+  for(int i:nums){
+    if (i==9) k++;
+  }
+  return k;
+}
+
+
+////////////////////////    9     ///////////////////////
+/*
+
+Given an array of ints, return true if one of the first 4 elements in the array is a 9. The array length may be less than 4.
+
+
+arrayFront9([1, 2, 9, 3, 4]) → true
+arrayFront9([1, 2, 3, 4, 9]) → false
+arrayFront9([1, 2, 3, 4, 5]) → false
+*/
+//////////////////////////////////////////////////////////
+public boolean arrayFront9(int[] nums) {
+  for(int i=0;i<nums.length;i++){
+    if (nums[i]==9) return true;
+    if(i==3 ) break;
+  }
+  return false;
+}
+
+////////////////////////    10     ///////////////////////
+/*
+Given an array of ints, return true if the sequence of numbers 1, 2, 3 appears in the array somewhere.
+
+
+array123([1, 1, 2, 3, 1]) → true
+array123([1, 1, 2, 4, 1]) → false
+array123([1, 1, 2, 1, 2, 3]) → true
+*/
+//////////////////////////////////////////////////////////
+public boolean array123(int[] nums) {
+  for(int i=0;i<nums.length-2;i++){
+    if(nums[i]==1&&nums[i+1]==2&&nums[i+2]==3) return true;
+  }
+  return false;
+}
+
+////////////////////////    11     ///////////////////////
+/*
+Given 2 strings, a and b, return the number of the positions where they contain the same length 2 substring. 
+So "xxcaazz" and "xxbaaz" yields 3, since the "xx", "aa", and "az" substrings appear in the same place in both strings.
+
+
+stringMatch("xxcaazz", "xxbaaz") → 3
+stringMatch("abc", "abc") → 2
+stringMatch("abc", "axc") → 0
+*/
+//////////////////////////////////////////////////////////
+public int stringMatch(String a, String b) {
+  int k=0;
+  int l=(a.length()>b.length())?b.length():a.length();
+  for(int i=0;i<(l-1);i++){
+    if(a.substring(i,i+2).equals(b.substring(i,i+2))) k++;
+  }
+  return k;
+}
+
+
+////////////////////////    12     ///////////////////////
+/*
+
+Given a string, return a version where all the "x" have been removed. Except an "x" at the very start or end should not be removed.
+
+
+stringX("xxHxix") → "xHix"
+stringX("abxxxcd") → "abcd"
+stringX("xabxxxcdx") → "xabcdx"
+*/
+//////////////////////////////////////////////////////////
+public String stringX(String str) {
+  String l="";
+  for(int i=0;i<str.length();i++){
+    if(i!=0&&i!=str.length()-1&&str.charAt(i) =='x'){ 
+     continue;
+    }
+     l+=str.charAt(i);
+  }
+  return l;
+}
+
+
+////////////////////////    13     ///////////////////////
+/*
+Given a string, return a string made of the chars at indexes 0,1, 4,5, 8,9 ... so "kittens" yields "kien".
+
+
+altPairs("kitten") → "kien"
+altPairs("Chocolate") → "Chole"
+altPairs("CodingHorror") → "Congrr"
+*/
+//////////////////////////////////////////////////////////
+public String altPairs(String str) {
+  String s="";
+  for (int i=0;i<str.length();i=i+4){
+    s+=str.charAt(i);
+    if(i+1==str.length()) break;
+    s+=str.charAt(i+1);
+  }
+  return s;
+}
+
+////////////////////////    14     ///////////////////////
+/*
+
+Given an array of ints, return true if it contains a 2, 7, 1 pattern:
+a value, followed by the value plus 5, followed by the value minus 1.
+Additionally the 271 counts even if the "1" differs by 2 or less from the correct value.
+
+
+has271([1, 2, 7, 1]) → true
+has271([1, 2, 8, 1]) → false
+has271([2, 7, 1]) → true
+*/
+//////////////////////////////////////////////////////////
+public boolean has271(int[] nums) {
+  for (int i=0;i<nums.length-2;i++){
+    int t=nums[i];
+    if(nums[i+1]==t+5&&Math.abs(t-nums[i+2]-1)<=2) return true;
+  }
+  return false;
+}
+
+
+
+
